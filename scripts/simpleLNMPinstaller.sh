38350bf2fb1c44ba3097c43b336d3da262f13ea9
Edi Septriyanto <me@masedi.net>
2017-04-29 00:53:39 +0700

    Fix readme

--- scripts/simpleLNMPinstaller.sh
+++ scripts/simpleLNMPinstaller.sh
@@ -78,7 +78,7 @@ if [[ "$DISTRIB_RELEASE" = "16.04" || "$DISTRIB_RELEASE" = "18" ]]; then
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3050AC3CD2AE6F03
 	sh -c "echo 'deb http://download.opensuse.org/repositories/home:/rtCamp:/EasyEngine/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/nginx-xenial.list"
 	# Add nginx service to systemd
-	wget --no-check-certificate https://gist.githubusercontent.com/joglomedia/3bb43ee9b17262f07dbe805aac3aee15/raw/d90c02a31c2873a0340b82554a4ec571568eb202/nginx.service -O /lib/systemd/system/nginx.service
+	#wget --no-check-certificate https://gist.githubusercontent.com/joglomedia/3bb43ee9b17262f07dbe805aac3aee15/raw/d90c02a31c2873a0340b82554a4ec571568eb202/nginx.service -O /lib/systemd/system/nginx.service
 else
 	# Ubuntu release 14.04
 	# https://rtcamp.com/wordpress-nginx/tutorials/single-site/fastcgi-cache-with-purging/
